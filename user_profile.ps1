4f3ef54a03fa31b89f262c77c0c7409041d41d37
ftmichael <70463155+ftmichael@users.noreply.github.com>
2023-02-09 14:15:22 +0700

    Update user_profile.ps1

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,41 +1,35 @@
 # Powershell Theme
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/paradox.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/dracula.omp.json" | Invoke-Expression
 
 # PowerShell UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
-# Posh git
+# Env
+$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
+
+# Posh Git
 Import-Module posh-git
 
 # Terminal Icons
 Import-Module -Name Terminal-Icons
 
-# z
+# Z
 Import-Module -Name z
 
 # PSReadLine
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
-Set-PSReadlineoption -PredictionViewStyle ListView
-
-# Fzf
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-
-# Env
-$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
+set-psreadlineoption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
 
 # Alias
-Set-Alias -Name v -Value nvim
-Set-Alias -Name vim -Value nvim
 Set-Alias ll ls
 Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig "C:\Program Files\Git\usr\bin\tig.exe"
 Set-Alias less "C:\Program Files\Git\usr\bin\less.exe"
 Set-Alias find "C:\Program Files\Git\usr\bin\find.exe"
-
-# Note
 # git config --global alias.co checkout
 # git config --global alias.br branch
 # git config --global alias.ci commit
