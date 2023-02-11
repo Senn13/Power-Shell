c880339bda012362fb432a34cfb1a1d91858b052
ftmichael <70463155+ftmichael@users.noreply.github.com>
2023-02-11 12:17:34 +0700

    Update user_profile.ps1

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,12 +1,6 @@
-# Powershell Theme
-$omp_config = Join-Path $PSScriptRoot ".\michael.omp.json"
+# Theme
+$omp_config = Join-Path $PSScriptRoot ".\ftmichael.omp.json"
 oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
-
-# PowerShell UTF-8
-[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
-
-# Env
-$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 
 # Posh Git
 Import-Module posh-git
@@ -20,22 +14,17 @@ Import-Module -Name z
 # PSReadLine
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
-set-psreadlineoption -PredictionViewStyle ListView
+set-psreadlineoption -PredictionViewStyle InlineView
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 
 # Alias
-Set-Alias c cls
 Set-Alias ll ls
 Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig "C:\Program Files\Git\usr\bin\tig.exe"
 Set-Alias less "C:\Program Files\Git\usr\bin\less.exe"
 Set-Alias find "C:\Program Files\Git\usr\bin\find.exe"
-# git config --global alias.co checkout
-# git config --global alias.br branch
-# git config --global alias.ci commit
-# git config --global alias.st status
 
 # Utilities
 function whereis ($command) {
